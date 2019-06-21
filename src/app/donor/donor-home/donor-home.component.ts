ee099ffada2b0d00f4f496fabd1f2a4fbd3d5007
abhinavb <abhinav.bhushan@lntinfotech.com>
2019-06-21 22:19:16 +0530

    21junecommit

---
+++ src/app/donor/donor-home/donor-home.component.ts
@@ -0,0 +1,171 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-donor-home',
+  templateUrl: './donor-home.component.html',
+  styleUrls: ['./donor-home.component.css']
+})
+
+export class DonorHomeComponent implements OnInit {
+  isDashboardClicked: boolean = true;
+  isRecordLegalClicked: boolean = false;
+  isInitiateAccountReceivable: boolean = false;
+  isGenerateBillClicked: boolean = false;
+  isProcessPaymentClicked: boolean = false;
+  isUserRoleClicked: boolean = false;
+  isPaymentClicked: boolean = false;
+  isInitiatePledgeClicked: boolean = false;
+  isMatchPaymentClicked: boolean = false;
+  isMenuOpen = true;
+  submitted = false;
+  contentMargin = 240;
+  header: String = "Pledge";
+
+  //@Input() donorMessage: boolean;
+ 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+// Open Dashboard
+  openDashboard() {
+    this.header = "Pledge";
+    this.isDashboardClicked = true;
+    this.isRecordLegalClicked =  false;
+    this.isInitiateAccountReceivable = false;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isMatchPaymentClicked= false;
+    this.isUserRoleClicked = false;
+    this.isPaymentClicked = false;
+    this.isInitiatePledgeClicked = false;
+     
+   }
+
+   // Open Initiate Pledge
+   openInitiatePledge() {
+    this.header = "Initiate Pledge";
+    this.isInitiatePledgeClicked = true;
+    this.isDashboardClicked = false;
+    this.isRecordLegalClicked =  false;
+    this.isInitiateAccountReceivable = false;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isMatchPaymentClicked= false;
+    this.isUserRoleClicked = false;
+    
+  }
+
+   // Open Record Legal 
+  openRecordLegal(){
+    this.header = "Legal";
+    this.isDashboardClicked = false;
+    this.isRecordLegalClicked = true;
+    this.isInitiateAccountReceivable = false;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isUserRoleClicked = false;
+    this.isPaymentClicked = false;
+    this.isMatchPaymentClicked= false;
+    this.isInitiatePledgeClicked = false;
+  }
+
+
+  // Open Initiate Account Receivable 
+  openInitiateAccountReceivable() {
+    this.header = "Initiate Account Receivable";
+    this.isInitiatePledgeClicked = false;
+    this.isDashboardClicked = false;
+    this.isRecordLegalClicked =  false;
+    this.isInitiateAccountReceivable = true;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isMatchPaymentClicked= false;
+    this.isUserRoleClicked = false;
+  }
+
+  // Open Generate Bill 
+  openGenerateBill() {
+    this.header = "Generate Bill";
+    this.isInitiatePledgeClicked = false;
+    this.isDashboardClicked = false;
+    this.isRecordLegalClicked =  false;
+    this.isInitiateAccountReceivable = false;
+    this.isGenerateBillClicked = true;
+    this.isProcessPaymentClicked = false;
+    this.isMatchPaymentClicked= false;
+    this.isUserRoleClicked = false;
+  }
+  
+  // Open Process Payment 
+
+  openProcessPayment(){
+    this.header = "Payment View for Treasury";
+    this.isDashboardClicked = false;
+    this.isRecordLegalClicked = false;
+    this.isInitiateAccountReceivable = false;
+    this.isInitiatePledgeClicked = false;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = true;
+    this.isMatchPaymentClicked= false;
+    this.isUserRoleClicked = false;
+    this.isPaymentClicked = false;
+ }
+
+// Open Process Payment 
+
+openMatchPayment(){
+  this.header = "Match Payment";
+  this.isDashboardClicked = false;
+  this.isRecordLegalClicked = false;
+  this.isInitiateAccountReceivable = false;
+  this.isInitiatePledgeClicked = false;
+  this.isGenerateBillClicked = false;
+  this.isProcessPaymentClicked = false;
+  this.isUserRoleClicked = false;
+  this.isPaymentClicked = false;
+  this.isMatchPaymentClicked=true;
+}
+
+  // Open User Role 
+  openUserRole(){
+    this.header = "User Role";
+    this.isDashboardClicked = false;
+    this.isRecordLegalClicked = false;
+    this.isInitiateAccountReceivable = false;
+    this.isInitiatePledgeClicked = false;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isUserRoleClicked = true;
+    this.isPaymentClicked = false;
+    this.isMatchPaymentClicked=false;
+  }
+  
+  openPayment(){
+  	this.header = "Payment";
+   	this.isDashboardClicked = false;
+   	this.isRecordLegalClicked = false;
+     this.isInitiateAccountReceivable = false;
+     this.isInitiatePledgeClicked = false;
+    this.isGenerateBillClicked = false;
+    this.isProcessPaymentClicked = false;
+    this.isUserRoleClicked = false;
+     this.isPaymentClicked = true;
+     this.isMatchPaymentClicked=false;
+  }
+
+  toggle() {
+    console.log("ismenuopen", this.isMenuOpen);
+
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (!this.isMenuOpen) {
+      this.contentMargin = 100;
+    } else {
+      this.contentMargin = 240;
+    }
+  }
+}
+
---
+++ src/app/donor/record-legal-donor/record-legal-donor.component.ts
@@ -0,0 +1,230 @@
+import { Component, OnInit, ViewChild, Input, Inject, Output, ChangeDetectorRef } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LegalData } from 'src/app/models/legal.model';
+import { LegalService } from 'src/app/services/legal.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatTabGroup } from '@angular/material';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+// import { Adal6Service } from 'adal-angular6';
+import { PledgeData } from 'src/app/models/pledge.model';
+import { PagerService } from 'src/app/services/pagerService.service';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+@Component({
+  selector: 'app-record-legal-donor',
+  templateUrl: './record-legal-donor.component.html',
+  styleUrls: ['./record-legal-donor.component.css']
+})
+export class RecordLegalComponentDonor implements OnInit {
+  submitted = false;
+  wbgBusinessUserSign: SignaturePad;
+  displayedColumns: string[] = ['pledgeId', 'wbgProgram', 'pledgeType', 'donorName', 'country', 'amount', 'approver', 'startDate', 'endDate', 'status', 'recordlegalBtn',];
+  dataSource: MatTableDataSource<PledgeData>;
+  selection = new SelectionModel<LegalData>(true, []);
+  isRowClicked: boolean = false;
+  rowData;
+ 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(SignaturePad) signaturePad: SignaturePad;
+  @Input() childMessage: boolean;
+  recordLegalForm: FormGroup;
+  searchForm: FormGroup;
+  private allItems: any[];
+  pager: any = {};
+  pagedItems: any[];
+
+
+  constructor( private legalService: LegalService,public dialog: MatDialog, private formBuilder: FormBuilder, private http: HttpClient,  private pagerService : PagerService, private changeDetectorRefs: ChangeDetectorRef, private spinnerService: Ng4LoadingSpinnerService) { }
+
+pageload(){
+  this.spinnerService.show();
+  this.searchForm = this.formBuilder.group({
+    pledgeId: [''],
+    pledgeFundType: [''],
+    startDate: [''],
+    endDate: [''],
+    programName: [''],
+    paymentPeriod: [''],
+    amount: [''],
+    installments:['']
+
+  });
+
+  this.legalService.getAllLegalrecords().subscribe(
+    (data) => {
+      this.spinnerService.hide();
+      this.allItems = data;
+      this.setPage(1);
+    },
+    (error) => {
+      console.log("Error: " + error)
+    });
+
+  this.recordLegalForm = this.formBuilder.group({
+    pledgeId: [''],
+    donorName: [''],
+    wbg_program: [''],
+    country: [''],
+    pledgeFundType: [''],
+    startDate: [''],
+    endDate: [''],
+    amount: [''],
+    paymentPeriod: [''],
+    noOfPayment: [''],
+    donorSign: ['',Validators.required],
+    wbgBusinessUserSign: ['']
+  });
+}
+  ngOnInit() {
+    console.log("childmsg legal", this.childMessage);
+   this.pageload(); 
+  }
+
+  setPage(page: number) { 
+    if (page < 1 || page > this.pager.totalPages) {
+        return;
+    }
+
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.allItems.length, page);
+
+    // get current page of items
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    console.log("Table Data", this.pagedItems)
+    this.dataSource = new MatTableDataSource(this.pagedItems);
+    this.dataSource.sort = this.sort;
+    this.changeDetectorRefs.detectChanges();
+}
+
+
+  selectRow(row) {
+    this.isRowClicked = true;
+    this.rowData = row;
+    console.log("row details", row, this.rowData);
+   }
+      
+  onCancel() {
+    this.isRowClicked = false;
+    this.pageload();
+  }
+
+  selectRowBtn() {
+   	console.log("row details-BTN", this.rowData)
+    console.log("1")
+  }
+
+  onLegalSubmit(row) {
+    this.isRowClicked = true;
+    this.rowData = row;
+    this.spinnerService.show();
+    console.log("legal submit clicked", row);
+    this.recordLegalForm.value.pledgeId = row.pledgeId;
+    this.recordLegalForm.value.donorName = row.donorName;
+    this.recordLegalForm.value.status = "Legal Recorded by Donor";
+    this.recordLegalForm.value.donorSign = this.signaturePad.toDataURL();
+    if((this.signaturePad.toDataURL().length)>1600){
+    var legalFormData = this.recordLegalForm.value;
+    this.legalService.onLegalSubmitDonor (legalFormData) 
+    .subscribe(response => {
+        console.log(response);
+        if(response === "SUCCESS"){
+          var dialogData = "Success: Legal document signed by donor successfully."
+        }
+        if(response === "FAILURE"){
+          var dialogData = "Failed: Legal document creation by donor failed."
+        }
+        this.spinnerService.hide();
+        const dialogRef = this.dialog.open(LegalCreatedDialogDonor, {
+          height: '250px',
+          width: '350px',
+          data: dialogData
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          console.log('The dialog was closed');
+          this.isRowClicked = false;
+    	  this.pageload();
+        }); 
+      }, (err: HttpErrorResponse) => {
+        this.spinnerService.hide();
+      const dialogRef = this.dialog.open(LegalCreatedDialogDonor, {
+        height: '250px',
+        width: '350px',
+        data: err.message
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        this.isRowClicked = false;
+        console.log('The dialog was closed');
+      });
+        console.log(err);
+        this.isRowClicked = false;
+    this.pageload();
+      });
+    this.recordLegalForm.reset();
+    this.signaturePad.clear();  
+  }
+  else{
+    const dialogRef = this.dialog.open(LegalCreatedDialogDonor, {
+      data: 'Please sign the legal agreement'
+    });
+
+    this.signaturePad.clear();
+  }
+}
+
+cancelLegal() {
+  this.pageload();
+  this.searchForm.reset();
+}
+
+searchLegal() {
+  var searchFormData = this.searchForm.value;
+  console.log("inside legal search",searchFormData);
+  console.log("inside legal search",this.searchForm.value);
+ this.legalService.search(searchFormData).subscribe(response => {
+   console.log("search", response, typeof (response));
+   this.allItems = response;
+   console.log(response)
+   this.setPage(1);
+ }, (err: HttpErrorResponse) => {
+   console.log(err);
+ });
+}
+
+
+}
+
+@Component({
+  selector: 'recordLegal.component',
+  templateUrl: 'recordLegaldialog-donor.component.html',
+  styles:[
+
+    `h1{background-color:#D9E9F5; border-bottom: 1px solid #2497F2 ;}
+    
+    p{font-style:Italic;font-style:bold;font-color:black} ;
+    overflow:none;
+  `
+   ]
+})
+export class LegalCreatedDialogDonor {
+  constructor(
+    public dialogRef: MatDialogRef<LegalCreatedDialogDonor>,
+    @Inject(MAT_DIALOG_DATA) public data) {
+  }
+  onClick() {
+    console.log("DATA", this.data);
+    this.dialogRef.close();
+
+    
+  }
+}
+
+
+
+
+
+
+
+

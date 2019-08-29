008c462f38bad5e61d4f9b9819aa2ea48cea88e0
SAKSHI KUMAR <Sakshi.Kumar@lntinfotech.com>
2019-08-29 18:15:31 +0530

    ui-29-8

---
+++ src/app/services/process-invoice.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { ProcessInvoice } from '../models/process-invoice.model';
+import { environment } from '../../environments/environment';
+import { AuthTokenService } from './authToken.service';
+import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
+
+@Injectable()
+export class ProcessInvoiceService {
+
+  public url: string = environment.processInvoiceServiceURL;
+  public pollerLambdaUrl: string = environment.pollerlambda;
+  public headers = environment.headers;
+  authtoken: string;
+  role_Object;
+  roleObject;
+
+  constructor(private http: HttpClient, private authService: AuthTokenService, private authenticationService: MsAdalAngular6Service) {
+    this.authService.getAuthToken().subscribe(response => {
+      this.authtoken = response;
+    });
+
+    this.role_Object=localStorage.getItem('Role');
+    localStorage.removeItem('Role');
+    this.roleObject=JSON.parse( this.role_Object);
+   }
+
+  getAllBillGenerated(): Observable<ProcessInvoice[]> {
+    return this.http.get<ProcessInvoice[]>(this.url+'/generateBillandInvoice/getAllInvoiceBillGenerated', { headers: new HttpHeaders().set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`)});
+  }
+
+  searchInvoice(postObject){
+    return this.http.post<any[]>(this.url+'/generateBillandInvoice/searchProcessInvoice', postObject, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`)});
+  }
+
+  submitInvoice(postObject): Observable<Object> {
+    return this.http.post(this.pollerLambdaUrl, postObject, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`)});
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,38 @@
+import { HttpHeaders } from '@angular/common/http';
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+/* export const environment = {
+  production: false
+}; */
+
+export const environment = {
+  production: false,
+  azureService : 'https://wbg-bpm-apim.azure-api.net',
+  awsService : 'https://5ufe1v6q92.execute-api.us-east-1.amazonaws.com/test',
+ pledgeServiceURL: 'https://wbgeabpmservicesaz.worldbankgroup.org',
+  legalServiceURL: 'https://wbgeabpmservicesalbaz.worldbankgroup.org',
+  accountReceivableServiceURL: 'https://wbgeabpmservicesalbaws.worldbankgroup.org',
+  billingServiceURL:'https://wbgeabpmservicesalbaws.worldbankgroup.org',
+  processInvoiceServiceURL : 'https://wbgeabpmservicesalbaws.worldbankgroup.org',
+  processPaymentServiceURL: 'https://wbgeabpmservicesalbaws.worldbankgroup.org',
+  matchPaymentServiceURL: 'https://wbgeabpmservicesalbaws.worldbankgroup.org',
+  userRoleServiceURL:'https://wbgeabpmservicesprem.worldbankgroup.org:9090',
+  auditAzure:'https://wbgeabpmwebalb.worldbankgroup.org',
+  auditAWS:'https://wbgeabpmservicesalbaws.worldbankgroup.org',
+  initiatorlambda :'https://wbgeabpmservicesaws.worldbankgroup.org/initiator',
+  pollerlambda : 'https://wbgeabpmservicesaws.worldbankgroup.org/poller',
+  headers : { headers: new HttpHeaders().set('Azure-Auth', 'true') },
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.

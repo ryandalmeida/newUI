bbf02471aa08d08996bb0f0ef8143b3b1f5392a3
SAKSHI KUMAR <Sakshi.Kumar@lntinfotech.com>
2019-09-09 17:18:59 +0530

    ui-9-9

---
+++ src/app/admin/audit-logDisplay/audit-logDisplay.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatDialog } from '@angular/material';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuditLogData } from 'src/app/models/audit.log.model';
+import { AuditLogDisplayService } from '../../services/auditLogDisplay.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { PagerService } from 'src/app/services/pagerService.service';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { SortService } from 'src/app/services/sortService.service';
+
+@Component({
+  selector: 'app-audit-logDisplay',
+  templateUrl: './audit-logDisplay.component.html',
+  styleUrls: ['./audit-logDisplay.component.css']
+})
+
+export class AuditLogDisplayComponent implements OnInit {
+  searchForm: FormGroup;
+  displayedColumns: string[] = ['auditLogId', 'action', 'content', 'modifiedBy', 'modifiedDate'];
+  dataSource: MatTableDataSource<AuditLogData>;
+  allItems: any[];
+  pagedItems: any[];
+  pager: any = {};
+  auditLogDataAzure: any[];
+  auditLogDataAWS: any[];
+  key: string = 'startDate';
+  order: boolean = false;
+  constructor(private auditLogDisplayService: AuditLogDisplayService, private formBuilder: FormBuilder, private http: HttpClient, public dialog: MatDialog, private pagerService: PagerService, private spinnerService: Ng4LoadingSpinnerService, private sortService: SortService) { }
+
+  ngOnInit() {
+    this.pageLoad();
+    this.searchForm = this.formBuilder.group({
+      action: [''],
+      modifiedBy: [''],
+      modifiedDate: ['']
+    });
+
+  }
+
+  pageLoad() {
+    this.spinnerService.show();
+    this.auditLogDisplayService.getAllAuditAzure().subscribe(
+      (data) => {
+        console.log("get pledge", data);
+        this.spinnerService.hide();
+        this.allItems = data;
+        this.tableSort('startDate');
+      },
+      (err: HttpErrorResponse) => {
+        console.log("Error: " , err);
+        this.spinnerService.hide();
+      });
+    
+  }
+
+  tableSort(key) {
+    this.key = key;
+    this.order = !this.order;
+    this.allItems = this.sortService.tableSort(this.key, this.order, this.allItems);
+    this.setPage(1);
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+    this.pager = this.pagerService.getPager(this.allItems.length, page);
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    this.dataSource = new MatTableDataSource(this.pagedItems);
+  }
+
+  searchAuditLog() {
+    var searchFormData = this.searchForm.value;
+    if(searchFormData.modifiedBy == ""){
+      searchFormData.modifiedBy = null;
+    }
+    if(searchFormData.modifiedDate == ""){
+      searchFormData.modifiedDate = null;
+    }
+    if(searchFormData.action == "" || searchFormData.action == null){
+      searchFormData.action = null;
+    }else{
+      searchFormData.action = (searchFormData.action).toUpperCase();
+    }
+    this.spinnerService.show();
+    console.log(searchFormData)
+    this.auditLogDisplayService.searchAuditLogData(searchFormData).subscribe(response => {
+      this.spinnerService.hide();
+      this.allItems = response;
+        this.tableSort('startDate');
+    }, (err: HttpErrorResponse) => {
+      this.spinnerService.hide();
+      console.log("ERROR:",err);
+    });
+  }
+
+  cancelSearch(){
+    this.pageLoad();
+  }
+}
+
---
+++ src/app/helpers/authInterceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
+import { AuthTokenService } from '../services/authToken.service';
+import { UserRoleService } from '../services/userRole.service';
+@Injectable()
+export class AuthTokenInterceptor implements HttpInterceptor {
+    authtoken: string;
+    role_Object;
+    roleObject;
+    userRole: any[] = [];
+    roles:string;
+    constructor(private authenticationService: MsAdalAngular6Service, private authService: AuthTokenService, private userRoleService: UserRoleService) {
+        this.role_Object=localStorage.getItem('Role');
+       // localStorage.removeItem('Role');
+        this.roleObject=JSON.parse( this.role_Object);
+     }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log("inside intercept")
+        this.authService.getAuthToken().subscribe(response => {
+            this.authtoken = response;
+        });
+
+        if (this.authenticationService.isAuthenticated) {
+            let headers;
+            if (request.headers.has('azure-auth')) {
+                headers = request.headers.delete('azure-auth').set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`);
+            } 
+            headers ? request = request.clone({ headers }) : null;
+        }
+        return next.handle(request);
+    }
+}

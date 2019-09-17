b01fa743053e7d1eef6e72fe442beafbf971c423
SAKSHI KUMAR <Sakshi.Kumar@lntinfotech.com>
2019-09-17 15:18:27 +0530

    ui-17-9

---
+++ src/app/services/billing.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Billing } from '../models/billing.model';
+import { environment } from '../../environments/environment.prod';
+import { AuthTokenService } from './authToken.service';
+import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
+
+@Injectable()
+export class BillingService {
+ 
+  public url: string = environment.billingServiceURL;
+  public pollerLambdaUrl: string = environment.pollerlambda;
+  public headers = environment.headers;
+  authtoken: string;
+  role_Object;
+  roleObject;
+
+  constructor(private http: HttpClient, private authService: AuthTokenService, private authenticationService: MsAdalAngular6Service) { 
+    this.authService.getAuthToken().subscribe(response => {
+      this.authtoken = response;
+    });
+
+    this.role_Object=sessionStorage.getItem('Role');
+    this.roleObject=JSON.parse( this.role_Object);
+  }
+
+  getAllAR(): Observable<Billing[]> {
+    return this.http.get<Billing[]>(this.url+'/accountReceivable/getActiveAR', { headers: new HttpHeaders().set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`)});
+  }
+
+  search(searchFormData) {
+    return this.http.post<Billing[]>(this.url+'/generateBillandInvoice/searchGenerateBilling', searchFormData,{ headers: new HttpHeaders().set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`)});
+  }
+
+  submitBill(submitObject): Observable<Object> {
+    return this.http.post(this.pollerLambdaUrl, submitObject, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.authtoken}`).set('ROLES', `${this.roleObject}`).set('uniqueName',`${this.authenticationService.userInfo.profile.unique_name}`)});
+  }
+}
